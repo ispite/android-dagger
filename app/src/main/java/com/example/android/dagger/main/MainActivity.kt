8bcba47496f7c2ae081aba234ad8104c5e47cc20
Denis Solovyov <infinityx93@gmail.com>
2023-01-19 22:31:08 +0300

    multiple activities with the same scope, completed 13 point. equal to 3_dagger_app branch

--- app/src/main/java/com/example/android/dagger/main/MainActivity.kt
+++ app/src/main/java/com/example/android/dagger/main/MainActivity.kt
@@ -26,15 +26,11 @@ import com.example.android.dagger.R
 import com.example.android.dagger.login.LoginActivity
 import com.example.android.dagger.registration.RegistrationActivity
 import com.example.android.dagger.settings.SettingsActivity
-import com.example.android.dagger.user.UserManager
 import javax.inject.Inject
 
 class MainActivity : AppCompatActivity() {
 
     // @Inject annotated fields will be provided by Dagger
-    @Inject
-    lateinit var userManager: UserManager
-
     @Inject
     lateinit var mainViewModel: MainViewModel
 
@@ -44,9 +40,9 @@ class MainActivity : AppCompatActivity() {
      * else carry on with MainActivity.
      */
     override fun onCreate(savedInstanceState: Bundle?) {
-        (application as MyApplication).appComponent.inject(this)
         super.onCreate(savedInstanceState)
 
+        val userManager = (application as MyApplication).appComponent.userManager()
         if (!userManager.isUserLoggedIn()) {
             if (!userManager.isUserRegistered()) {
                 startActivity(Intent(this, RegistrationActivity::class.java))
@@ -57,6 +53,10 @@ class MainActivity : AppCompatActivity() {
             }
         } else {
             setContentView(R.layout.activity_main)
+
+            // If the MainActivity needs to be displayed, we get the UserComponent
+            // from the application graph and gets this Activity injected
+            userManager.userComponent!!.inject(this)
             setupViews()
         }
     }
